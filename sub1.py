4c9641ca9ae1f05219e76761dc0ecf8e547cc8bc
zzhh777 <785172991@qq.com>
2016-10-07 20:00:37 +0800

    线性回归1
    特征为预测目标前1个小时每10分钟的平均连接(6维)，前一天该时段前半小时后半小时每10分钟平均连接(6维)。粗试用了线性回归，GBDT(100颗树)，带核线性回归(默认核)。两种线性回归比GBDT好大约2%。

---
+++ sub1.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+from sklearn import cross_validation
+from sklearn import linear_model
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.ensemble import GradientBoostingRegressor
+
+wifi = pd.read_csv('data/wifi_AP_2D.csv')
+wifi_t = wifi.drop(['WIFIAPTag'], axis = 1)
+for i in [x for x in range(144, 553) if (x % 20 == 0) & (x != 0)]:
+    if i not in [200, 320, 340, 360, 480, 500]:
+        try:
+            X1 = wifi_t.ix[:, i - 6 : i]
+            X1 = np.append(wifi_t.ix[:, i - 144 - 3 : i - 144 + 4], X1, axis = 1)
+            X = np.append(X, X1, axis = 0)
+        except:
+            X = wifi_t.ix[:, i - 6 : i]
+            X = np.append(wifi_t.ix[:, i - 144 - 3 : i - 144 + 4], X, axis = 1)
+
+train_X = X[:, : 12]
+train_Y = X[:, 12]
+print(train_X.shape)
+
+clf = linear_model.LinearRegression()
+clf.fit(train_X, train_Y)
+# score = cross_validation.cross_val_score(clf, train_X, train_Y, cv = 5)
+# print(clf.coef_)
+# print("LR:" + str(score.mean()))
+#
+# clf = KernelRidge(alpha=1.0)
+# clf.fit(train_X, train_Y)
+# score = cross_validation.cross_val_score(clf, train_X, train_Y, cv = 5)
+# print(clf.coef0)
+# print("KR:" + str(score.mean()))
+#
+# clf = GradientBoostingRegressor(n_estimators = 100, learning_rate=0.1)
+# clf.fit(train_X, train_Y)
+# score = cross_validation.cross_val_score(clf, train_X, train_Y, cv = 5)
+# print(clf.feature_importances_)
+# print("GBDT:" + str(score.mean()))
+
+for i in range(553, 570):
+    print(i)
+    X = wifi_t.ix[:, i - 5 : i + 1]
+    X = np.append(wifi_t.ix[:, i - 144 - 2 : i - 144 + 5], X, axis = 1)
+    wifi_t[i] = clf.predict(X)
+
+wifi_t.ix[553:].to_csv('res.csv', index = False)
